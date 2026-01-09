53bdc3dbf8e8aacf44f90bba42e6729fbf6c86ea
Michael Raspuzzi <michael@raspuzzi.com>
2026-01-08 22:35:04 -0800

    Initial project set up with greeting

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// startup research memo agent for deep tech companies from vinod khoslas firm
+
+// startup research memo agent for deep tech companies from vinod khoslas firm
+
+console.log('Hello from startup research memo agent for deep tech companies from vinod khoslas firm!');
+
